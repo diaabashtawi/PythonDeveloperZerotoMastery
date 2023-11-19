788fdd386a552888e24dcdba92efa644c4d6e4fb
diaabashtawi <diaabashtawi@gmail.com>
2023-11-19 16:57:23 +0400

    adding basic package and list.py file and adding some code to cheatSheet package

---
+++ basic/list.py
@@ -0,0 +1,15 @@
+li1 = [1, 2, 3, 4, 5]
+li2 = ['a', 'b', 'c']
+li3 = [1, 2, 'b', True]
+
+
+# List slicing
+amazon_cart = [
+    'notebooks',
+    'SSD',
+    'iPhone 13',
+    'Dell laptop',
+    'ipad 11',
+    'Airpods'
+]
+print(amazon_cart[0:2])
